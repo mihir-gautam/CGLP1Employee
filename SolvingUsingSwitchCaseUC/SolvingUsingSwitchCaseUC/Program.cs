249bd89e022f037cd47191e68ae9e6c5cb2365cb
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-09-27 20:55:49 +0530

    [Mihir] Add Solving method by switch command Use Case

---
+++ SolvingUsingSwitchCaseUC/SolvingUsingSwitchCaseUC/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SolvingUsingSwitchCaseUC
+{
+    class Program
+    {
+        public String presence(int status)
+        {   
+
+            switch(status)
+            {
+                case 1:
+                    return "Employee Present Full Time";
+                    
+                case 2:
+                    return "Employee is working part time";
+                    
+                case 3:
+                    return "Employee is absent";
+                   
+                default:
+                    throw new InvalidOperationException("status must be in range 1-3");
+
+            } 
+
+            
+        }
+    }
+}
